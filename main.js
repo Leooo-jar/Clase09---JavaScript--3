c2c88672acfbc9cec96956ddacc74977996ba55f
Leonid <gringoobussines@gmail.com>
2025-08-21 19:32:37 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+/* function esPar(numero) {
+  if (numero % 2 === 0) {
+    return true;
+    console.log("El número es par");
+  } else {
+    return false;
+    console.log("El número es impar");
+  }
+} */
+
+if (hora > 18) {
+  alert("Esta cerrado, es de tarde");
+} else if (hora >= 9) {
+  alert("Esta cerrado, es de mañana");
+} else {
+  alert("Esta cerrado, es de noche");
+}
